c159b9a4971c5e93ec8d0e837688e29fa119e0b1
ghadiyayashsvi <ghadiyayashsvi@gmail.com>
2025-05-02 18:08:52 +0530

    2/5/25

---
+++ lab work 2.1/Qus-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int number, square;
+
+    
+    printf("Enter a number: ");
+    scanf("%d", &number);
+	quare = number * number ;
+
+    
+  printf("| The square is: %-6d |\n", square);
+    
+    return 0;
+}
+
