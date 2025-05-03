111314455e592edf15490c7db05c225b13febea7
ghadiyayashsvi <ghadiyayashsvi@gmail.com>
2025-05-03 15:46:01 +0530

    3/5/25

---
+++ PR-3- Control statements/- Control statements (break, continue, and goto)/PR-3 control statements (break , continue,and got).cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+main(){
+	int n;
+	printf("enter your mark out of = 100");
+	sancf("%d",&n);
+	if(n>90)	{
+	("you gat a grade");
+	}else if("n>80 && n<=90"){
+	}
+
+		printf("you gat b grade");
+	
+	}
+	
+	eals if ("n>70 && n<=80"){
+		printf("you gat c grade");
+	}
+	
+	eals if ("n>50 && n<=60"){
+		printf("you gat e grade");
+	}
+		eals if ("n>40 && n<=50"){
+		printf("you gat f grade");
+	}
+	eals{
+		printf("you are fail");
+	}
+}
+
+
+
+
+
+	
+
+
