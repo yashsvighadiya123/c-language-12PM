40849c22ba37c4c3af2c6f1903018ef8696fc9c9
unknown <ghadiyayashsvi>
2025-05-12 15:08:29 +0530

    12/5/25

---
+++ Array in Detail/qus-2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ main(){
+ 	int i,n;
+ 
+	printf("enter array size :-");
+ 	scanf("%d",&n);
+ 	int a[n];
+ 	for(i=0;i<n;i++)
+ 	{
+ 		printf("a[%d]:-",i);
+ 		scanf("%d",&a[i]);
+ 		
+	 }
+	 printf("\n printf event array element mixmum\n");
+	 for(i=0;i<n;i++){
+	 	if(a[i]%2==0){
+	 		printf("%d ",a[i]);
+		 }
+	 }
+ 	
+ 	
+ }
